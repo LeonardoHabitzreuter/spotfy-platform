6b774875f29b80b30276559a8ca4e7908c4fc863
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-12-08 11:11:45 -0200

    configured bootstrapp

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import React from 'react'
 import styles from './app.styl'
 
 const App = () => (
-  <h1 className={styles.blue}>See all your favorite artists, albums and songs</h1>
+  <div>
+    <h1 className={styles.blue}>See all your favorite artists, albums and songs</h1>
+    <h3 className='bg-danger'>Let's look around</h3>
+  </div>
 )
 
 export default App
