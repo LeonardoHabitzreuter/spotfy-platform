7965a82723e8a5a2a568c4ff6c06297216da06d3
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-12-16 12:11:21 -0200

    using browserLocation at widget component

--- src/components/browserLocation/index.js
+++ src/components/browserLocation/index.js
@@ -1,8 +1,24 @@
+import { pipe, concat, when, includes, not } from 'ramda'
+
+const goTo = pathName => window.location.replace(pathName)
+
+const changePathName = pipe(
+  when(
+    pipe(
+      includes('/'),
+      not
+    ),
+    concat('/')
+  ),
+  concat(window.location.origin),
+  goTo
+)
+
 const BrowserLocation = props => (
   props.render({
-    goTo: window.location.replace,
+    goTo,
     pathname: window.location.pathname,
-    changePathName: pathName => window.location.replace(`${window.location.origin}${pathName}`)
+    changePathName
   })
 )
 
--- src/home/components/widget/index.js
+++ src/home/components/widget/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-const reloadPage = key => {
-  window.location.href = key
-}
+import BrowserLocation from 'components/browserLocation'
 
 export const Widget = ({ id, title, text, children, className }) => (
-  <article className={classNames('jumbotron', 'jumbotron-fluid', className)} onClick={() => reloadPage(id)}>
-    {children}
-    <h1 id='title' className='display-4'>{title}</h1>
-    <p id='text' className='lead'>{text}</p>
-  </article>
+  <BrowserLocation render={({ changePathName }) => (
+    <article className={classNames('jumbotron', 'jumbotron-fluid', className)} onClick={() => changePathName(id)}>
+      {children}
+      <h1 id='title' className='display-4'>{title}</h1>
+      <p id='text' className='lead'>{text}</p>
+    </article>
+  )} />
 )
 
 Widget.propTypes = {
